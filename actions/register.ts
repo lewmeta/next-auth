dd28156dc5f6bf488a4a066a638b1a3d3d4fe655
thegypsyboyo <lewisonyango9@gmail.com>
2024-07-29 20:20:01 +0300

    connected prisma and created mock user with server actions

--- actions/register.ts
+++ actions/register.ts
@@ -2,6 +2,9 @@
 
 import { RegisterSchema } from "@/schemas";
 import * as z from "zod"
+import bcrypt from "bcrypt"
+import { db } from "@/lib/db";
+import { getUserByEmail } from "@/data/user";
 
 export const register = async (values: z.infer<typeof RegisterSchema>) => {
     const validatedFields = RegisterSchema.safeParse(values);
@@ -10,5 +13,23 @@ export const register = async (values: z.infer<typeof RegisterSchema>) => {
         return { error: "Invalid filds!" };
     }
 
-    return { success: "Email sent!" };
+    const { email, password, name } = validatedFields.data;
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const existingUser = await getUserByEmail(email);
+
+    if (existingUser) {
+        return { error: "Email already in use!" };
+    }
+
+    await db.user.create({
+        data: {
+            name,
+            email,
+            password: hashedPassword,
+        },
+    });
+
+    //TODO:send varification token; 
+    return { success: "User created!" };
 }
