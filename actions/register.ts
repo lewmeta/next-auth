a070df69b21ba2cbe3aa56b5a4806433ee4cbb18
thegypsyboyo <lewisonyango9@gmail.com>
2024-07-31 13:06:02 +0300

    added verification email with the help of resend

--- actions/register.ts
+++ actions/register.ts
@@ -6,6 +6,7 @@ import bcrypt from "bcryptjs"
 import { db } from "@/lib/db";
 import { getUserByEmail } from "@/data/user";
 import { generateVerificationToken } from "@/lib/tokens";
+import { sendVerificationEmail } from "@/lib/mail";
 
 export const register = async (values: z.infer<typeof RegisterSchema>) => {
     const validatedFields = RegisterSchema.safeParse(values);
@@ -31,8 +32,15 @@ export const register = async (values: z.infer<typeof RegisterSchema>) => {
         },
     });
 
-    //TODO:send varification token; 
+
+    /**
+     * Send varification token; 
+     */
     const verificationToken = await generateVerificationToken(email);
+    await sendVerificationEmail(
+        verificationToken.email,
+        verificationToken.token,
+    )
 
     return { success: "A confirmation email has been sent!" };
 }
