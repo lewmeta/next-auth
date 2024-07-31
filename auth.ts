1097d393a25d1c8d87465386ce8ba2655c0bc33f
thegypsyboyo <lewisonyango9@gmail.com>
2024-07-31 10:27:24 +0300

    added out own pages to auth instead of the default nextauth error for social logins

--- auth.ts
+++ auth.ts
@@ -25,6 +25,10 @@ export const {
     signIn,
     signOut,
 } = NextAuth({
+    pages: {
+        signIn: "/auth/login",
+        error: "/auth/error",
+    },
     events: {
         async linkAccount({ user }) {
             await db.user.update({
--- components/auth/login-form.tsx
+++ components/auth/login-form.tsx
@@ -2,6 +2,7 @@
 
 import { useForm } from "react-hook-form"
 import { zodResolver } from "@hookform/resolvers/zod"
+import { useSearchParams } from "next/navigation"
 import * as z from "zod"
 import { useState, useTransition } from "react"
 
@@ -24,6 +25,12 @@ import { FormSuccess } from "@/components/form-success"
 import { login } from "@/actions/login"
 
 export const LoginForm = () => {
+    const searchParams = useSearchParams();
+
+    const urlError = searchParams.get("error") === "OAuthAccountNotLinked"
+        ? "Email already in use with a different provider!"
+        : ""
+
     const [isPending, startTransition] = useTransition();
     const [error, setError] = useState<string | undefined>("")
     const [success, setSuccess] = useState<string | undefined>("")
@@ -103,7 +110,7 @@ export const LoginForm = () => {
                             )}
                         />
                     </div>
-                    <FormError message={error} />
+                    <FormError message={error || urlError} />
                     <FormSuccess message={success} />
                     <Button
                         type="submit"
