549806264b284eabfdcf65b80244ee2d1df3fa09
thegypsyboyo <lewisonyango9@gmail.com>
2024-07-31 14:20:54 +0300

    we have enabled email verification and created a form for it

---
+++ app/auth/new-verification/page.tsx
@@ -0,0 +1,11 @@
+import { NewVerificationForm } from "@/components/auth/new-verification-form"
+
+const NewVerification = () => {
+    return (
+        <div>
+            <NewVerificationForm />
+        </div>
+    )
+}
+
+export default NewVerification
---
+++ components/auth/new-verification-form.tsx
@@ -0,0 +1,61 @@
+"use client"
+
+import { useState } from "react"
+import { useCallback, useEffect } from "react"
+import { BeatLoader } from "react-spinners"
+import { useSearchParams } from "next/navigation"
+
+import { newVerification } from "@/actions/new-verification"
+import { CardWrapper } from "@/components/auth/card-wrapper"
+import { FormError } from "@/components/form-error"
+import { FormSuccess } from "@/components/form-success"
+
+export const NewVerificationForm = () => {
+    const [error, setError] = useState<string | undefined>("");
+    const [success, setSuccess] = useState<string | undefined>("");
+
+    const searchParams = useSearchParams();
+
+    const token = searchParams.get("token");
+
+    const onSubmit = useCallback(() => {
+        if (success || error) return;
+        if (!token) {
+            setError("Missing token");
+
+            return
+        }
+
+        newVerification(token)
+            .then((data) => {
+                setSuccess(data.success);
+                setError(data.error);
+            })
+    }, [token, success, error]);
+
+    useEffect(() => {
+        onSubmit();
+    }, [onSubmit]);
+
+    return (
+        <CardWrapper
+            headerLabel="Confirm your verification"
+            backButtonHref="/auth/login"
+            backButtonLabel="Back to login"
+        >
+            <div className="flex items-center w-full justify-center">
+                {!success && !error && (
+                    <BeatLoader />
+                )}
+                {!success && (
+                    <FormError
+                        message={error}
+                    />
+                )}
+                <FormSuccess
+                    message={success}
+                />
+            </div>
+        </CardWrapper>
+    )
+}
